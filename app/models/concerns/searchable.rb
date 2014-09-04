6a4c8a0ebfb098111793a9f148bfde21a73182c0
francescomaturi <francesco_maturi@tiscali.it>
2014-09-04 14:04:30 +0200

    fix encode and method locality name

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -35,37 +35,38 @@ module Searchable
       when 1
         return self.where(:name_encoded => encode(args[0])) if args[0].is_a?(String)
       when 2
-        return self.where("name_encoded = ? AND region_id = ?", encode(args[0]), args[1]) if args[0].is_a?(String) and args[1].is_a?(Integer)
+        if args[0].is_a?(String) and args[1].is_a?(Integer)
+          return self.where("name_encoded = ? AND pointable.region_id = ?", encode(args[0]), args[1]) if self.is_a?Point
+          return self.where("name_encoded = ? AND region_id = ?", encode(args[0]), args[1])
+        end
+        puts "Usage: #{self.class.name}.search(<name>) or #{self.class.name}.search(<name>,<region_id>)"
+        return nil
       end
-      puts "Usage: #{self.class.name}.search(<name>) or #{self.class.name}.search(<name>,<region_id>)"
-      return nil
     end
 
   end
 
-end
+  def encode name
+    name = name.downcase
+    name = name.split("/").first if name.match(/\//)
+    name = name.split("\\").first if name.match(/\\/)
+    name = name.split(" - ").first if name.match(/ - /)
 
-def encode name
-  name = name.downcase
-  name = name.split("/").first if name.match(/\//)
-  name = name.split("\\").first if name.match(/\\/)
-  name = name.split(" - ").first if name.match(/ - /)
-  
-  while name != encode1(name) do
-    name = encode1(name)
+    while name != encode1(name) do
+      name = encode1(name)
+    end
+    # only at the end remove spaces
+    name = name.gsub(/[^0-9A-Za-z]/, '')
+    return name
   end
-  # only at the end remove spaces
-  name = name.gsub(/[^0-9A-Za-z]/, '')
-  return name
-end
 
-def encode1 name
-  # parole e congiunzioni preceduti e seguiti da uno spazio o all inizio della riga
-  name = name.gsub(/(^|\s)(e|a|al|d|di|de|del|dell|dello|delle|della|degli|da|dal|dall|dalla|dallo|dei|du|san|sant|sant[o|a|i])\s/, ' ') 
-  name = name.gsub(/(^|\s)(d|du|del|dal|dell|dall|san|sant)['-]\s?/,' ') # il ? sta per 0 o 1
-  name = name.gsub(/(^|\s)s\./,' ')
-  return name
-end
+  def encode1 name
+    # parole e congiunzioni preceduti e seguiti da uno spazio o all inizio della riga
+    name = name.gsub(/(^|\s)(e|a|al|d|di|de|del|dell|dello|delle|della|degli|da|dal|dall|dalla|dallo|dei|du|san|sant|sant[o|a|i])\s/, ' ') 
+    name = name.gsub(/(^|\s)(d|du|del|dal|dell|dall|san|sant)['-]\s?/,' ') # il ? sta per 0 o 1
+    name = name.gsub(/(^|\s)s\./,' ')
+    return name
+  end
 
 # questa è quella vecchia just to know
 # def encode name
@@ -79,8 +80,4 @@ end
 #   name = name.gsub(/[^0-9A-Za-z]/, '')
 #   return name
 # end
-
-
-
-
 
